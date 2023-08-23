bd91249506191bf96b674e5623358e6bf283dd49
datttrian <datttrian@gmail.com>
2023-08-23 02:19:11 +0000

    Initial commit

---
+++ scrape.r
@@ -0,0 +1,9 @@
+library(rvest)
+library(tidyverse)
+units = read_html("https://forum.duome.eu/viewtopic.php?p=57747-searchable-word-overview-for-french-path-204-units-4883-words#p57747") %>%
+    html_nodes(xpath = '//div[@class="content"]/h1/a[2]') %>%
+    html_attr('href')
+
+df = units[1] %>% read_html() %>% html_nodes(xpath='//*[@id="post_content60754"]/div[1]/table') %>% html_table()
+
+df
